bebb6ab14ac27b493b9a5963be383281d28fa560
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-07-26 00:33:58 -0400

    Extend HandPoseController from HandPoseDetector.

--- addons/hand_pose_detector/hand_pose_controller.gd
+++ addons/hand_pose_detector/hand_pose_controller.gd
@@ -1,6 +1,6 @@
 @tool
 class_name HandPoseController
-extends Node
+extends HandPoseDetector
 
 
 ## Hand Pose Controller Node
@@ -57,66 +57,72 @@ const _POSE_TRANSFORMS_RIGHT : Array[Transform3D] = [
 ]
 
 
+@export_group("Controller", "controller_")
+
 ## Name for the virtual controller tracker
-@export var tracker_name : String = "/user/hand_pose_controller/left"
+@export var controller_tracker_name : String = "/user/hand_pose_controller/left"
 
 ## Pose type
-@export var pose_type : PoseType = PoseType.SKELETON
+@export var controller_pose_type : PoseType = PoseType.SKELETON
 
 ## Hand poses generating boolean values
-@export var action_map : HandPoseActionMap
+@export var controller_action_map : HandPoseActionMap
 
-
-## Hand Pose Detector
-var pose_detector : HandPoseDetector
 
 ## Controller Tracker
-var tracker : XRControllerTracker
+var controller_tracker : XRControllerTracker
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Call the base
+	super()
+
 	# Skip if in editor
 	if Engine.is_editor_hint():
 		set_process(false)
 		return
 
-	# Get the pose detector
-	pose_detector = get_parent() as HandPoseDetector
-	if not pose_detector:
-		set_process(false)
-		return
+	# If the hand-pose-set is not specified then construct one dynamically
+	# from the controller action map.
+	if not hand_pose_set and controller_action_map:
+		hand_pose_set = HandPoseSet.new()
+		for action in controller_action_map.actions:
+			hand_pose_set.poses.append(action.pose)
 
 	# Subscribe to the detector events
-	pose_detector.pose_started.connect(_pose_started)
-	pose_detector.pose_ended.connect(_pose_ended)
+	pose_started.connect(_pose_started)
+	pose_ended.connect(_pose_ended)
 
 	# Create the controller tracker
-	tracker = XRControllerTracker.new()
-	tracker.name = tracker_name
-	XRServer.add_tracker(tracker)
+	controller_tracker = XRControllerTracker.new()
+	controller_tracker.name = controller_tracker_name
+	XRServer.add_tracker(controller_tracker)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	# Call the base
+	super(delta)
+
 	# Skip if no trackers
-	if not pose_detector.tracker or not tracker:
+	if not hand_tracker or not controller_tracker:
 		return
 
 	# Get the hand tracker pose
-	var pose := pose_detector.tracker.get_pose(&"default")
+	var pose := hand_tracker.get_pose(&"default")
 	if not pose:
 		return
 
 	# Get the conversion transform
-	var hand := pose_detector.tracker.hand
+	var hand := hand_tracker.hand
 
 	# Get the conversion transform
 	var conv_xform : Transform3D
 	if hand == XRPositionalTracker.TrackerHand.TRACKER_HAND_LEFT:
-		conv_xform = _POSE_TRANSFORMS_LEFT[pose_type]
+		conv_xform = _POSE_TRANSFORMS_LEFT[controller_pose_type]
 	else:
-		conv_xform = _POSE_TRANSFORMS_RIGHT[pose_type]
+		conv_xform = _POSE_TRANSFORMS_RIGHT[controller_pose_type]
 
 	# Apply conversion to pose components
 	var pose_transform := pose.transform * conv_xform
@@ -124,8 +130,8 @@ func _process(_delta: float) -> void:
 	var pose_angular := _rotate_angular_velocity(pose.angular_velocity, conv_xform.basis)
 
 	# Update the controller tracker pose
-	tracker.hand = hand
-	tracker.set_pose(
+	controller_tracker.hand = hand
+	controller_tracker.set_pose(
 		pose.name,
 		pose_transform,
 		pose_linear,
@@ -135,23 +141,20 @@ func _process(_delta: float) -> void:
 
 # Customize properties
 func _validate_property(property: Dictionary) -> void:
-	if property.name == "tracker_name":
+	if property.name == "controller_tracker_name":
 		property.hint = PROPERTY_HINT_ENUM_SUGGESTION
 		property.hint_string = "/user/hand_pose_controller/left,/user/hand_pose_controller/right"
+	else:
+		super(property)
 
 
 # Get configuration warnings
 func _get_configuration_warnings() -> PackedStringArray:
-	var warnings := PackedStringArray()
+	var warnings := super()
 
-	# Verify tracker name is set
-	if tracker_name == "":
-		warnings.append("Tracker name not set")
-
-	# Verify parent pose
-	var parent_pose_detector := get_parent() as HandPoseDetector
-	if not parent_pose_detector:
-		warnings.append("Must be child of HandPoseDetector node")
+	# Verify controller tracker name is set
+	if controller_tracker_name == "":
+		warnings.append("Controller racker name not set")
 
 	# Return the warnings
 	return warnings
@@ -160,37 +163,37 @@ func _get_configuration_warnings() -> PackedStringArray:
 # Handle start of pose
 func _pose_started(p_name : String) -> void:
 	# Skip if no tracker or action map
-	if not tracker or not action_map:
+	if not controller_tracker or not controller_action_map:
 		return
 
 	# Find the action
-	var action := action_map.get_action(p_name)
+	var action := controller_action_map.get_action(p_name)
 	if not action:
 		return
 
 	# Set the input
 	if action.action_type == HandPoseAction.ActionType.BOOL:
-		tracker.set_input(action.action_name, true)
+		controller_tracker.set_input(action.action_name, true)
 	else:
-		tracker.set_input(action.action_name, 1.0)
+		controller_tracker.set_input(action.action_name, 1.0)
 
 
 # Handle end of pose
 func _pose_ended(p_name : String) -> void:
 	# Skip if no tracker or action map
-	if not tracker or not action_map:
+	if not controller_tracker or not controller_action_map:
 		return
 
 	# Find the action
-	var action := action_map.get_action(p_name)
+	var action := controller_action_map.get_action(p_name)
 	if not action:
 		return
 
 	# Set the input
 	if action.action_type == HandPoseAction.ActionType.BOOL:
-		tracker.set_input(action.action_name, false)
+		controller_tracker.set_input(action.action_name, false)
 	else:
-		tracker.set_input(action.action_name, 0.0)
+		controller_tracker.set_input(action.action_name, 0.0)
 
 
 # Returns an angular velocity rotated by the given basis matrix.
